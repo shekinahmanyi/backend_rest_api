89384ee786bfe9c5016c0699f925e46c367950f9
shekinahmanyi <shekinahmanyi@gmail.com>
2023-06-19 22:29:01 +0100

    Installed Moongoose Installed mongodb connected to the database changed file name

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:sheki@cluster0.jdydyme.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected Successfully to Database and listening on port 5000")
+  )
+  .catch((err) => console.log(err));
